064307ebd99bcf06900430b68563d650bf4cf119
Ranja Func <seolin9340@gmail.com>
2021-04-30 11:35:09 +0900

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ const cheerio = require("cheerio");
 const logger = require('morgan');
 const axios = require('axios');
 const app = express();
-const PORT = 8000;
+const PORT = 8080;
+
+// ** MIDDLEWARE ** //
+// const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.herokuapp.com']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     console.log("** Origin of request " + origin)
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       console.log("Origin acceptable")
+//       callback(null, true)
+//     } else {
+//       console.log("Origin rejected")
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
 const lostarkUrl = 'https://lostark.game.onstove.com/Profile/Character/';
 app.use(
@@ -100,6 +115,15 @@ app.post('/search', (req, res) => {
         })
         .then((data) => res.send(data));    
 });
+
+// if (process.env.NODE_ENV === 'production') {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//   // Handle React routing, return all requests to React app
+//     app.get('*', function(req, res) {
+//       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//     });
+//   }
 
 app.listen(PORT, () => console.log(`listening on the port ${PORT}`));
             
